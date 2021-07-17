aa2e5fe68ad4478751b27c53fad368f8d13d0058
wep21 <border_goldenmarket@yahoo.co.jp>
2021-07-18 04:16:01 +0900

    Apply lint
    Signed-off-by: wep21 <border_goldenmarket@yahoo.co.jp>

--- include/ars408_ros/ars408_constants.hpp
+++ include/ars408_ros/ars408_constants.hpp
@@ -1,73 +1,84 @@
+// Copyright 2021 Perception Engine, Inc. All rights reserved.
 //
-// Copyright 2021. Perception Engine Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-#ifndef PE_ARS408_ROS_ARS408_CONSTANTS_H
-#define PE_ARS408_ROS_ARS408_CONSTANTS_H
+#ifndef ARS408_ROS__ARS408_CONSTANTS_HPP_
+#define ARS408_ROS__ARS408_CONSTANTS_HPP_
 
 namespace ars408
 {
-  const uint32_t RADAR_CFG = 0x200;
-  const uint8_t  RADAR_CFG_BYTES = 8;
+const uint32_t RADAR_CFG = 0x200;
+const uint8_t RADAR_CFG_BYTES = 8;
 
-  const uint32_t RADAR_STATE = 0x201;
-  const uint8_t  RADAR_STATE_BYTES = 8;
+const uint32_t RADAR_STATE = 0x201;
+const uint8_t RADAR_STATE_BYTES = 8;
 
-  const uint32_t FILTER_CFG = 0x202;
-  const uint8_t  FILTER_CFG_BYTES = 8;
+const uint32_t FILTER_CFG = 0x202;
+const uint8_t FILTER_CFG_BYTES = 8;
 
-  const uint32_t FILTER_STATE_HEADER = 0x203;
-  const uint8_t  FILTER_STATE_HEADER_BYTES = 2;
+const uint32_t FILTER_STATE_HEADER = 0x203;
+const uint8_t FILTER_STATE_HEADER_BYTES = 2;
 
-  const uint32_t FILER_STATE_CFG = 0x204;
-  const uint8_t  FILER_STATE_CFG_BYTES = 5;
+const uint32_t FILER_STATE_CFG = 0x204;
+const uint8_t FILER_STATE_CFG_BYTES = 5;
 
-  const uint32_t COLL_DET_CFG = 0x400;
-  const uint8_t  COLL_DET_CFG_BYTES = 2;
+const uint32_t COLL_DET_CFG = 0x400;
+const uint8_t COLL_DET_CFG_BYTES = 2;
 
-  const uint32_t COLL_DET_REGION_CFG = 0x401;
-  const uint8_t  COLL_DET_REGION_CFG_BYTES = 8;
+const uint32_t COLL_DET_REGION_CFG = 0x401;
+const uint8_t COLL_DET_REGION_CFG_BYTES = 8;
 
-  const uint32_t COLL_DET_STATE = 0x408;
-  const uint8_t  COLL_DET_STATE_BYTES = 4;
+const uint32_t COLL_DET_STATE = 0x408;
+const uint8_t COLL_DET_STATE_BYTES = 4;
 
-  const uint32_t COLL_DET_REGION_STATE = 0x402;
-  const uint8_t  COLL_DET_REGION_STATE_BYTES = 8;
+const uint32_t COLL_DET_REGION_STATE = 0x402;
+const uint8_t COLL_DET_REGION_STATE_BYTES = 8;
 
-  const uint32_t SPEED_INFORMATION = 0x300;
-  const uint8_t  SPEED_INFORMATION_BYTES = 2;
+const uint32_t SPEED_INFORMATION = 0x300;
+const uint8_t SPEED_INFORMATION_BYTES = 2;
 
-  const uint32_t YAW_RATE_INFORMATION = 0x301;
-  const uint8_t  YAW_RATE_INFORMATION_BYTES = 2;
+const uint32_t YAW_RATE_INFORMATION = 0x301;
+const uint8_t YAW_RATE_INFORMATION_BYTES = 2;
 
-  const uint32_t CLUSTER_STATUS = 0x600;
-  const uint8_t  CLUSTER_STATUS_BYTES = 5;
+const uint32_t CLUSTER_STATUS = 0x600;
+const uint8_t CLUSTER_STATUS_BYTES = 5;
 
-  const uint32_t CLUSTER_GENERAL = 0x701;
-  const uint8_t  CLUSTER_GENERAL_BYTES = 8;
+const uint32_t CLUSTER_GENERAL = 0x701;
+const uint8_t CLUSTER_GENERAL_BYTES = 8;
 
-  const uint32_t CLUSTER_QUALITY = 0x702;
-  const uint8_t  CLUSTER_QUALITY_BYTES = 5;
+const uint32_t CLUSTER_QUALITY = 0x702;
+const uint8_t CLUSTER_QUALITY_BYTES = 5;
 
-  const uint32_t OBJ_STATUS = 0x60A;
-  const uint8_t  OBJ_STATUS_BYTES = 4;
+const uint32_t OBJ_STATUS = 0x60A;
+const uint8_t OBJ_STATUS_BYTES = 4;
 
-  const uint32_t OBJ_GENERAL = 0x60B;
-  const uint8_t  OBJ_GENERAL_BYTES = 8;
+const uint32_t OBJ_GENERAL = 0x60B;
+const uint8_t OBJ_GENERAL_BYTES = 8;
 
-  const uint32_t OBJ_QUALITY = 0x60C;
-  const uint8_t  OBJ_QUALITY_BYTES = 7;
+const uint32_t OBJ_QUALITY = 0x60C;
+const uint8_t OBJ_QUALITY_BYTES = 7;
 
-  const uint32_t OBJ_EXTENDED = 0x60D;
-  const uint8_t  OBJ_EXTENDED_BYTES = 8;
+const uint32_t OBJ_EXTENDED = 0x60D;
+const uint8_t OBJ_EXTENDED_BYTES = 8;
 
-  const uint32_t OBJ_WARNING = 0x60E;
-  const uint8_t  OBJ_WARNING_BYTES = 4;
+const uint32_t OBJ_WARNING = 0x60E;
+const uint8_t OBJ_WARNING_BYTES = 4;
 
-  const uint32_t VERSION_ID = 0x700;
-  const uint8_t  VERSION_ID_BYTES = 4;
+const uint32_t VERSION_ID = 0x700;
+const uint8_t VERSION_ID_BYTES = 4;
 
-  const uint32_t COLL_DET_RELAY_CTRL = 0x8;
-  const uint8_t  COLL_DET_RELAY_CTRL_BYTES = 1;
-}
-#endif //PE_ARS408_ROS_ARS408_CONSTANTS_H
+const uint32_t COLL_DET_RELAY_CTRL = 0x8;
+const uint8_t COLL_DET_RELAY_CTRL_BYTES = 1;
+}  // namespace ars408
+
+#endif  // ARS408_ROS__ARS408_CONSTANTS_HPP_
--- include/ars408_ros/ars408_object.hpp
+++ include/ars408_ros/ars408_object.hpp
@@ -1,101 +1,115 @@
-/*
- * Copyright 2021. Perception Engine Inc. All rights reserved.
- */
+// Copyright 2021 Perception Engine, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-#ifndef PE_ARS408_ROS_ARS408_OBJECT_H
-#define PE_ARS408_ROS_ARS408_OBJECT_H
+#ifndef ARS408_ROS__ARS408_OBJECT_HPP_
+#define ARS408_ROS__ARS408_OBJECT_HPP_
+
+#include <string>
+
 namespace ars408
 {
-  class RadarObject
+class RadarObject
+{
+public:
+  std::string ObjectClassToString()
   {
-  public:
-    std::string ObjectClassToString()
-    {
-      switch(object_class)
-      {
-        case ars408::Obj_3_Extended::ObjectClassProperty::WIDE:
-          return "WIDE ";
-          break;
-        case ars408::Obj_3_Extended::ObjectClassProperty::MOTORCYCLE:
-          return "MOTORCYCLE ";
-          break;
-        case ars408::Obj_3_Extended::ObjectClassProperty::BICYCLE:
-          return "BICYCLE ";
-          break;
-        case ars408::Obj_3_Extended::ObjectClassProperty::RESERVED_01:
-          return "RESERVED_01";
-          break;
-        case ars408::Obj_3_Extended::ObjectClassProperty::POINT:
-          return "POINT";
-          break;
-        case ars408::Obj_3_Extended::ObjectClassProperty::CAR:
-          return "CAR ";
-          break;
-        case ars408::Obj_3_Extended::ObjectClassProperty::RESERVED_02:
-          return "RESERVED_02";
-          break;
-        case ars408::Obj_3_Extended::ObjectClassProperty::TRUCK:
-          return "TRUCK";
-          break;
-        default:
-          return "UNDEFINED";
-          break;
-      }
+    switch (object_class) {
+      case ars408::Obj_3_Extended::ObjectClassProperty::WIDE:
+        return "WIDE ";
+        break;
+      case ars408::Obj_3_Extended::ObjectClassProperty::MOTORCYCLE:
+        return "MOTORCYCLE ";
+        break;
+      case ars408::Obj_3_Extended::ObjectClassProperty::BICYCLE:
+        return "BICYCLE ";
+        break;
+      case ars408::Obj_3_Extended::ObjectClassProperty::RESERVED_01:
+        return "RESERVED_01";
+        break;
+      case ars408::Obj_3_Extended::ObjectClassProperty::POINT:
+        return "POINT";
+        break;
+      case ars408::Obj_3_Extended::ObjectClassProperty::CAR:
+        return "CAR ";
+        break;
+      case ars408::Obj_3_Extended::ObjectClassProperty::RESERVED_02:
+        return "RESERVED_02";
+        break;
+      case ars408::Obj_3_Extended::ObjectClassProperty::TRUCK:
+        return "TRUCK";
+        break;
+      default:
+        return "UNDEFINED";
+        break;
     }
-    std::string DynamicPropertyToString()
-    {
-      switch(dynamic_property)
-      {
-        case ars408::Obj_1_General::DynamicProperty::MOVING:
-          return "MOVING";
-          break;
-        case ars408::Obj_1_General::DynamicProperty::STATIONARY:
-          return "STATIONARY";
-          break;
-        case ars408::Obj_1_General::DynamicProperty::ONCOMING:
-          return "ONCOMING";
-          break;
-        case ars408::Obj_1_General::DynamicProperty::CROSSING_LEFT:
-          return "CROSSING_LEFT";
-          break;
-        case ars408::Obj_1_General::DynamicProperty::CROSSING_RIGHT:
-          return "CROSSING_RIGHT";
-          break;
-        case ars408::Obj_1_General::DynamicProperty::UNKNOWN:
-          return "UNKNOWN";
-          break;
-        case ars408::Obj_1_General::DynamicProperty::STOPPED:
-          return "STOPPED";
-          break;
-        default:
-          return "UNDEFINED";
-          break;
-      }
+  }
+  std::string DynamicPropertyToString()
+  {
+    switch (dynamic_property) {
+      case ars408::Obj_1_General::DynamicProperty::MOVING:
+        return "MOVING";
+        break;
+      case ars408::Obj_1_General::DynamicProperty::STATIONARY:
+        return "STATIONARY";
+        break;
+      case ars408::Obj_1_General::DynamicProperty::ONCOMING:
+        return "ONCOMING";
+        break;
+      case ars408::Obj_1_General::DynamicProperty::CROSSING_LEFT:
+        return "CROSSING_LEFT";
+        break;
+      case ars408::Obj_1_General::DynamicProperty::CROSSING_RIGHT:
+        return "CROSSING_RIGHT";
+        break;
+      case ars408::Obj_1_General::DynamicProperty::UNKNOWN:
+        return "UNKNOWN";
+        break;
+      case ars408::Obj_1_General::DynamicProperty::STOPPED:
+        return "STOPPED";
+        break;
+      default:
+        return "UNDEFINED";
+        break;
     }
-    std::string ToString(){
-      std::ostringstream stream;
-      stream << "Sequence: " << sequence_id << ", ID: " << (unsigned int)id << ", X: " << distance_long_x
-             << ", Y: " << distance_lat_y << ", SpeedX: " << speed_long_x << ", SpeedY: " << speed_lat_y
-             << ", rcs: " << rcs  << ", Status: ";
-      stream << ObjectClassToString();
-      stream << std::endl;
-      return stream.str();
-    }
-    uint16_t sequence_id;
-    uint8_t id;
-    float distance_long_x;
-    float distance_lat_y;
-    float speed_long_x;
-    float speed_lat_y;
-    ars408::Obj_1_General::DynamicProperty dynamic_property;
-    float rcs;
-    float rel_acceleration_long_x;/*Relative acceleration in longitudinal direction in m/s^2 */
-    ars408::Obj_3_Extended::ObjectClassProperty object_class;
-    float rel_acceleration_lat_y;/*Relative acceleration in lateral direction in m/s^2*/
-    float orientation_angle;/*Orientation angle of the object in degrees*/
-    float length;/*Length of the tracked object*/
-    float width;/*Width of the tracked object*/
-    float probability_existence;
-  };
-}
-#endif //PE_ARS408_ROS_ARS408_OBJECT_H
+  }
+  std::string ToString()
+  {
+    std::ostringstream stream;
+    stream << "Sequence: " << sequence_id << ", ID: " << (unsigned int)id << ", X: " <<
+      distance_long_x <<
+      ", Y: " << distance_lat_y << ", SpeedX: " << speed_long_x << ", SpeedY: " << speed_lat_y <<
+      ", rcs: " << rcs << ", Status: ";
+    stream << ObjectClassToString();
+    stream << std::endl;
+    return stream.str();
+  }
+  uint16_t sequence_id;
+  uint8_t id;
+  float distance_long_x;
+  float distance_lat_y;
+  float speed_long_x;
+  float speed_lat_y;
+  ars408::Obj_1_General::DynamicProperty dynamic_property;
+  float rcs;
+  float rel_acceleration_long_x;  /*Relative acceleration in longitudinal direction in m/s^2 */
+  ars408::Obj_3_Extended::ObjectClassProperty object_class;
+  float rel_acceleration_lat_y;  /*Relative acceleration in lateral direction in m/s^2*/
+  float orientation_angle;  /*Orientation angle of the object in degrees*/
+  float length;  /*Length of the tracked object*/
+  float width;  /*Width of the tracked object*/
+  float probability_existence;
+};
+}  // namespace ars408
+
+#endif  // ARS408_ROS__ARS408_OBJECT_HPP_
