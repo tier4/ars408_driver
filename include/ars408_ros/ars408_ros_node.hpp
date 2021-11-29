8999f6dfdc17c251f69614360001098307c43cf0
Takeshi Miura <57553950+1222-takeshi@users.noreply.github.com>
2021-11-29 11:05:20 +0900

    port autoware_iv_msgs to autoware_auto_msgs (#9)

--- include/ars408_ros/ars408_ros_node.hpp
+++ include/ars408_ros/ars408_ros_node.hpp
@@ -15,51 +15,55 @@
 #ifndef ARS408_ROS__ARS408_ROS_NODE_HPP_
 #define ARS408_ROS__ARS408_ROS_NODE_HPP_
 
+#include "ars408_ros/ars408_driver.hpp"
+#include "rclcpp/rclcpp.hpp"
+
+#include "autoware_auto_perception_msgs/msg/detected_objects.hpp"
+#include "autoware_auto_perception_msgs/msg/tracked_objects.hpp"
+#include "can_msgs/msg/frame.hpp"
+#include "unique_identifier_msgs/msg/uuid.hpp"
+
 #include <random>
 #include <string>
 #include <unordered_map>
 #include <vector>
 
-#include "autoware_perception_msgs/msg/dynamic_object_array.hpp"
-#include "can_msgs/msg/frame.hpp"
-#include "rclcpp/rclcpp.hpp"
-#include "unique_identifier_msgs/msg/uuid.hpp"
-
-#include "ars408_ros/ars408_driver.hpp"
-
 class PeContinentalArs408Node : public rclcpp::Node
 {
   rclcpp::Subscription<can_msgs::msg::Frame>::SharedPtr subscriber_can_raw_;
   rclcpp::Subscription<can_msgs::msg::Frame>::SharedPtr subscription_;
-  rclcpp::Publisher<autoware_perception_msgs::msg::DynamicObjectArray>
-  ::SharedPtr publisher_dynamic_object_array_;
+  rclcpp::Publisher<autoware_auto_perception_msgs::msg::DetectedObjects>::SharedPtr
+    publisher_detected_objects_;
+  rclcpp::Publisher<autoware_auto_perception_msgs::msg::TrackedObjects>::SharedPtr
+    publisher_tracked_objects_;
 
   std::string output_frame_;
 
   const uint8_t max_radar_id = 255;
   std::vector<unique_identifier_msgs::msg::UUID> UUID_table_;
+  bool publish_tracked_object_;
 
-  ars408::Ars408Driver ars408_driver_ {};
+  ars408::Ars408Driver ars408_driver_{};
 
   void CanFrameCallback(const can_msgs::msg::Frame::SharedPtr can_msg);
 
   void GenerateUUIDTable();
 
-  autoware_perception_msgs::msg::DynamicObject
-  ConvertRadarObjectToAwDynamicObject(const ars408::RadarObject & in_object);
+  autoware_auto_perception_msgs::msg::DetectedObject ConvertRadarObjectToAwDetectedObject(
+    const ars408::RadarObject & in_object);
 
-  static uint32_t
-  ConvertRadarClassToAwSemanticClass(
+  autoware_auto_perception_msgs::msg::TrackedObject ConvertRadarObjectToAwTrackedObject(
+    const ars408::RadarObject & in_object);
+
+  static uint32_t ConvertRadarClassToAwSemanticClass(
     const ars408::Obj_3_Extended::ObjectClassProperty & in_radar_class);
 
-  static unique_identifier_msgs::msg::UUID
-  GenerateRandomUUID();
+  static unique_identifier_msgs::msg::UUID GenerateRandomUUID();
 
 public:
   explicit PeContinentalArs408Node(const rclcpp::NodeOptions & node_options);
   void RadarDetectedObjectsCallback(
-    const std::unordered_map<uint8_t,
-    ars408::RadarObject> & detected_objects);
+    const std::unordered_map<uint8_t, ars408::RadarObject> & detected_objects);
   void Run();
 };
 
