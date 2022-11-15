81faaa53e34910428cf37e74c0058050db3f5f07
scepter914 <scepter914@gmail.com>
2022-11-15 13:00:59 +0900

    add RadarScan interface
    Signed-off-by: scepter914 <scepter914@gmail.com>

--- src/ars408_ros_node.cpp
+++ src/ars408_ros_node.cpp
@@ -29,6 +29,7 @@ PeContinentalArs408Node::PeContinentalArs408Node(const rclcpp::NodeOptions & nod
 void PeContinentalArs408Node::CanFrameCallback(const can_msgs::msg::Frame::SharedPtr can_msg)
 {
   if (!can_msg->data.empty()) {
+    can_data_ = can_msg;
     ars408_driver_.Parse(can_msg->id, can_msg->data, can_msg->dlc);
   }
 }
@@ -84,20 +85,47 @@ radar_msgs::msg::RadarTrack PeContinentalArs408Node::ConvertRadarObjectToRadarTr
   return out_object;
 }
 
+radar_msgs::msg::RadarReturn PeContinentalArs408Node::ConvertRadarObjectToRadarReturn(
+  const ars408::RadarObject & in_object)
+{
+  radar_msgs::msg::RadarReturn radar_return;
+  radar_return.range = std::sqrt(
+    in_object.distance_long_x * in_object.distance_long_x +
+    in_object.distance_lat_y * in_object.distance_lat_y);
+  radar_return.azimuth = std::atan2(in_object.distance_lat_y, in_object.distance_long_x);
+  radar_return.doppler_velocity = in_object.rel_acceleration_long_x;
+  radar_return.elevation = 0.0;
+  radar_return.amplitude = 0.0;
+  return radar_return;
+}
+
 void PeContinentalArs408Node::RadarDetectedObjectsCallback(
   const std::unordered_map<uint8_t, ars408::RadarObject> & detected_objects)
 {
+  // rclcpp::Time current_time = this->get_clock()->now();
   radar_msgs::msg::RadarTracks output_objects;
   output_objects.header.frame_id = output_frame_;
+  output_objects.header.stamp = can_data_->header.stamp;
 
-  rclcpp::Time current_time = this->get_clock()->now();
-  output_objects.header.stamp = current_time;
+  radar_msgs::msg::RadarScan output_scan;
+  output_scan.header.frame_id = output_frame_;
+  output_scan.header.stamp = can_data_->header.stamp;
 
   for (const auto & object : detected_objects) {
-    radar_msgs::msg::RadarTrack object_ = ConvertRadarObjectToRadarTrack(object.second);
-    output_objects.tracks.emplace_back(object_);
+    if (publish_radar_track_) {
+      output_objects.tracks.emplace_back(ConvertRadarObjectToRadarTrack(object.second));
+    };
+    if (publish_radar_scan_) {
+      output_scan.returns.emplace_back(ConvertRadarObjectToRadarReturn(object.second));
+    }
   }
-  publisher_radar_tracks_->publish(output_objects);
+
+  if (publish_radar_track_) {
+    publisher_radar_tracks_->publish(output_objects);
+  }
+  if (publish_radar_scan_) {
+    publisher_radar_scan_->publish(output_scan);
+  }
 }
 
 unique_identifier_msgs::msg::UUID PeContinentalArs408Node::GenerateRandomUUID()
@@ -119,6 +147,8 @@ void PeContinentalArs408Node::GenerateUUIDTable()
 void PeContinentalArs408Node::Run()
 {
   output_frame_ = this->declare_parameter<std::string>("output_frame", "ars408");
+  publish_radar_track_ = this->declare_parameter<bool>("publish_radar_track", true);
+  publish_radar_scan_ = this->declare_parameter<bool>("publish_radar_scan", false);
 
   ars408_driver_.RegisterDetectedObjectsCallback(
     std::bind(&PeContinentalArs408Node::RadarDetectedObjectsCallback, this, std::placeholders::_1));
@@ -129,6 +159,7 @@ void PeContinentalArs408Node::Run()
 
   publisher_radar_tracks_ =
     this->create_publisher<radar_msgs::msg::RadarTracks>("~/output/objects", 10);
+  publisher_radar_scan_ = this->create_publisher<radar_msgs::msg::RadarScan>("~/output/scan", 10);
 }
 
 #include "rclcpp_components/register_node_macro.hpp"
